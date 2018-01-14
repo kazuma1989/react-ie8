afa0cf505a002829d06d20ef66a0fd644bcc621d
kazuma1989 <funifuni.1204@gmail.com>
2018-01-14 13:36:47 +0000

    Removed bable-polyfill to prevent stack overflow error in ie8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 JSON = require('imports?this=>window,this=>global!json3');
-require('babel-polyfill');
+require('core-js/es5');
+require('core-js/es6/object');
+require('core-js/es6/function');
+require('core-js/es6/array');
+require('core-js/es6/string');
+require('core-js/es6/promise');
 
 (() => {
   const text = JSON.stringify([1, 2, 3].map(x => x * 10)).repeat(2);
@@ -10,4 +15,4 @@ require('babel-polyfill');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>HELLO</h1>, document.getElementById('root'));
